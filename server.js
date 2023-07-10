91eab7f21b94e1b7ce4bd25c9a6cf92153324341
Itsannm <ann.mwanzia@student.moringaschool.com>
2023-07-10 12:19:21 +0300

    Redo

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = 3000;
 
 // Set up routes
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/recipes', async (req, res) => {
+  try {
+    const { query } = req.query;
+    const apiKey = 'YOUR_SPOONACULAR_API_KEY';
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json(data.results);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/recipes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const apiKey = 'YOUR_SPOONACULAR_API_KEY';
+    const apiUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Start the server
